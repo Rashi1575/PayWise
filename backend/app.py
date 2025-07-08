0ac70fa89adfdf7748a8a21fb25660e51b8b5ac1
Kpish <138367639+Kpishh@users.noreply.github.com>
2025-07-09 01:23:39 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -277,4 +277,8 @@ def get_payments():
     return jsonify({"success": True, "payments": result.data})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
