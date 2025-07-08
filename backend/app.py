b3efd5cab3f28cb3294d99639cafd8164b2f5c64
Kpish <138367639+Kpishh@users.noreply.github.com>
2025-07-09 00:36:08 +0530

    Update app.py temp

--- backend/app.py
+++ backend/app.py
@@ -280,4 +280,7 @@ def get_payments():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
